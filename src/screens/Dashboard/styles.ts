6fc9262bc51fdf688781dd8522fc9efee50d787d
olivpuddin <bububeatriz@icloud.com>
2022-04-13 14:39:53 -0300

    feat: finished dashboard components and stylized

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,55 +1,108 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { FlatList, FlatListProps } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
 
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { DataListProps } from '.';
+
+
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
-    width: 100%;
-    height: ${RFPercentage(42)}px;
+  width: 100%;
+  height: ${RFPercentage(42)}px;
 
-    background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
-    width: 100%;
+  width: 100%;
 
-    padding: 0 24px;
+  padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
-    flex-direction: row;
-    align-items: center;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const Photo = styled.Image`
-    width: ${RFValue(48)}px;
-    height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
 
-    border-radius: 10px;
+  border-radius: 10px;
 `;
 
 export const User = styled.View`
-    margin-left: 17px;
+  margin-left: 17px;
 `;
 
 export const UserGreeting = styled.Text`
-    color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.shape};
 
-    font-size:  ${RFValue(18)}px;
-    font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
-    color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.shape};
 
-    font-size:  ${RFValue(18)}px;
-    font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
 `;
+
+export const Icon = styled<any>(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightsCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1%;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new (props: FlatListProps<DataListProps>) => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;
